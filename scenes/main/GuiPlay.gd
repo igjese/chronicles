8f70885352da119eec4ac4cfee23684928168636
Igor Jese <igor@mockupscreens.com>
2024-04-27 20:57:31 +0200

    debug menu - Hand: give card into hand 1, Challenge: pick challenge to activate

--- scenes/main/GuiPlay.gd
+++ scenes/main/GuiPlay.gd
@@ -7,9 +7,12 @@ extends Control
 @onready var sound_punch = get_node("/root/Main/Sounds/Punch")
 @onready var sm = get_node("/root/Main/StateLoop").sm
 
+var helpers = Helpers.new()
+
 func _ready():
     Game.resources_updated.connect(refresh_statusbar)
     Game.statuses_updated.connect(refresh_statusbar)
+    $CheatValue.get_popup().index_pressed.connect(apply_cheat)
 
 
 func _on_btn_exit_pressed():
@@ -31,15 +34,15 @@ func update_resource_display(resource_node, resource_name, delay):
     var old_value = int(resource_node.text)
     if Game[resource_name] != old_value:
         await get_tree().create_timer(0.2).timeout
-        if Game[resource_name] > old_value:
-            sound_coin.play()
-        elif Game[resource_name] < old_value:
-            sound_punch.play()
         resource_node.text = str(Game[resource_name])
         var tween = create_tween()
         tween.tween_property(resource_node, "scale", Vector2(1.5, 1.5), delay/2).set_ease(Tween.EASE_IN)
         tween.tween_property(resource_node, "scale", Vector2(1, 1), delay/2).set_ease(Tween.EASE_OUT)
-
+        await get_tree().create_timer(delay/2).timeout
+        if Game[resource_name] > old_value:
+            sound_coin.play()
+        elif Game[resource_name] < old_value:
+            sound_punch.play()
 
 func on_card_clicked(card):
     print("card clicked: ", card.card_name)
@@ -49,3 +52,27 @@ func on_card_clicked(card):
 func on_card_right_clicked(card):
     print("card right-clicked: ", card.card_name)
             
+
+func _on_cheat_action_item_selected(index):
+    var cards = []
+    match index:
+        0: cards = helpers.get_cards_by_type("Action")
+        1: cards = helpers.get_cards_by_type("History")
+    var popup = $CheatValue.get_popup()
+    popup.clear()
+    for card in cards:
+        popup.add_item(card["name"])
+        
+        
+func apply_cheat(index):
+    var card_name = $CheatValue.get_popup().get_item_text(index)
+    var state_loop = get_node("/root/Main/StateLoop")
+    match $CheatAction.get_item_index($CheatAction.get_selected_id()):
+        0: # Put action card into Hand1
+            helpers.spawn_card(Game.cards_by_name[card_name], get_node("/root/Main/Hand/Hand1/cards"),CardScene.FACE_UP)
+        1: # Put history card on Challenge top and run it
+            helpers.spawn_card(Game.cards_by_name[card_name], get_node("/root/Main/Challenge/cards"),CardScene.FACE_UP)
+            Game.card_stack.clear()
+            Game.effect_stack.clear()
+            sm.change_state(state_loop.ACTIVATE_CHALLENGE)
+
